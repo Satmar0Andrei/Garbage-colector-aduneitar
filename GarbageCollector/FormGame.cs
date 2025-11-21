dc46ebf51a5a712ea020115c48661e29e79d1e46
Otniel <fatu.otniel@gmail.com>
2025-11-21 20:08:29 +0200

    Remove unnecessary usings

--- GarbageCollector/FormGame.cs
+++ GarbageCollector/FormGame.cs
@@ -1,17 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Xml.Linq;
-using static System.Net.Mime.MediaTypeNames;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
 using Image = System.Drawing.Image;
 
 namespace GarbageCollector
