3b150900b516461adc03ad5f138a4f8c47d66f9d
Otniel <fatu.otniel@gmail.com>
2025-11-21 20:54:21 +0200

    Added Exit Button in Tutorial Form. Changed LabelText From Tutorial Form

--- GarbageCollector/Tutorial.Designer.cs
+++ GarbageCollector/Tutorial.Designer.cs
@@ -32,6 +32,7 @@
             this.pnlFundal = new System.Windows.Forms.Panel();
             this.lblText = new System.Windows.Forms.Label();
             this.lblTitlu = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
             this.pnlFundal.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -65,15 +66,29 @@
             this.lblTitlu.TabIndex = 0;
             this.lblTitlu.Text = "Tutorial";
             // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Honeydew;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnExit.Location = new System.Drawing.Point(239, 527);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(149, 67);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
             // Tutorial
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(634, 611);
+            this.Controls.Add(this.btnExit);
             this.Controls.Add(this.pnlFundal);
             this.DoubleBuffered = true;
             this.Name = "Tutorial";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Tutorial";
             this.pnlFundal.ResumeLayout(false);
             this.pnlFundal.PerformLayout();
@@ -86,5 +101,6 @@
         private System.Windows.Forms.Panel pnlFundal;
         private System.Windows.Forms.Label lblText;
         private System.Windows.Forms.Label lblTitlu;
+        private System.Windows.Forms.Button btnExit;
     }
 }
--- GarbageCollector/Tutorial.cs
+++ GarbageCollector/Tutorial.cs
@@ -14,5 +14,10 @@ namespace GarbageCollector
         {
 
         }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
